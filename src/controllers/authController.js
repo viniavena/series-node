dda51b74f6bc4f8b6ee7ea5102b7896da8cb371d
Vini Avena <admin@admins-MacBook-Air.local>
2021-07-04 18:18:14 -0300

    password encrypt & email register validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,8 +5,16 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+  const { email } = req.body;
+
   try {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: 'Email já cadastrado' });
+    }
+
     const user = await User.create(req.body);
+
+    user.password = undefined;
 
     return res.send({ user });
   } catch (err) {
