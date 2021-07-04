a58a00caf1c6fab006f850da29d13b5b5c610444
Vini Avena <admin@admins-MacBook-Air.local>
2021-07-04 18:36:52 -0300

    login route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
@@ -21,6 +22,20 @@ router.post('/register', async (req, res) => {
     console.log(err);
     return res.status(400).send({ error: 'Erro ao efetuar cadastro!' });
   }
+});
+
+router.post('/token', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) return res.status(400).send({ error: 'Usuário não encontrado' });
+
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(400).send({ error: 'Senha inválida' });
+
+  user.password = undefined;
+
+  res.send({ user });
 });
 
 module.exports = app => app.use('/auth', router);
